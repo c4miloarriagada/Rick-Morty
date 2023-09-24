fe1edf3ab6c4359683930aff7141a81bab85c4f8
c4miloarriagada <camilo.arriagadav@gmail.com>
2023-09-24 17:07:26 -0300

    hf: animations

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 export const Title = () => {
   return (
     
-      <div class="animate-[fadeIn_1s]    bg-[url('https://res.cloudinary.com/dwtkwakbc/image/upload/v1695165533/Rick-and-Morty_noy1ks.png')] h-full bg-contain w-full bg-center bg-no-repeat  flex drop-shadow-xl ">
+      <div class=" bg-[url('https://res.cloudinary.com/dwtkwakbc/image/upload/v1695165533/Rick-and-Morty_noy1ks.png')] h-full bg-contain w-full bg-center bg-no-repeat  flex drop-shadow-xl ">
     
       </div>
     
